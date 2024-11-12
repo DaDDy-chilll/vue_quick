6215e4da8bd5d1964d692d727f713b6b949fec21
daddy-chill <blacklistuser420@gmail.com>
2024-11-12 19:50:56 +0630

    start vue

---
+++ vue_app/src/App2.vue
@@ -0,0 +1,52 @@
+<script >
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status:"inactive",
+      tasks:['task1','task2','task3'],
+      link:'https://www.google.com'
+    }
+  },
+  methods: {
+    changeStatus() {
+      if(this.status === 'active') {
+        this.status = 'pending'
+      } else if(this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">user is pending</p>
+  <p v-else>user is inactive</p>
+
+  <!-- v-for -->
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+
+  <!-- v-bind -->
+  <!-- <a v-bind:href="link">Google</a> -->
+  <a :href="link">Google</a>
+
+  <!-- v-on -->
+   <br>
+  <!-- <button v-on:click="changeStatus">Change Status</button> -->
+  <button @click="changeStatus">Change Status</button>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ vue_app/src/App3.vue
@@ -0,0 +1,53 @@
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+      const name = ref('John Doe');
+      const status = ref('active');
+      const tasks = ref(['task1', 'task2', 'task3']);
+
+      const changeStatus = () => {
+        if(status.value === 'active') {
+        status.value = 'pending'
+      } else if(status.value === 'pending') {
+        status.value = 'inactive'
+      } else {
+        status.value = 'active'
+      }
+      }
+
+      return {
+        name,
+        status,
+        tasks,
+        changeStatus
+      }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">user is pending</p>
+  <p v-else>user is inactive</p>
+
+  <!-- v-for -->
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+
+  <!-- v-on -->
+   <br>
+  <!-- <button v-on:click="changeStatus">Change Status</button> -->
+  <button @click="changeStatus">Change Status</button>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
