22a0459be01bae18bb5abd0c52daa4b09e1c990f
daddy-chill <blacklistuser420@gmail.com>
2024-11-13 19:43:17 +0630

    update changes

---
+++ vue_app/src/test_exe/App4.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref,onMounted } from "vue";
+const name = ref('John Doe');
+const status = ref('active');
+const tasks = ref(['task1', 'task2', 'task3']);
+const newTask =ref('')
+const changeStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending'
+  } else if (status.value === 'pending') {
+    status.value = 'inactive'
+  } else {
+    status.value = 'active'
+  }
+}
+
+const addTask = () => {
+  if(newTask.value.trim() !== '') { 
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
+
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1)
+}
+
+onMounted( async () => {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products')
+    const data = await response.json()
+    tasks.value = data.map(task => task.title)
+  } catch (error) {
+    console.error('Error fetching tasks:', error)
+  }
+})
+
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">user is pending</p>
+  <p v-else>user is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">AddNew Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit" @click="addTask">Add Task</button>
+  </form>
+
+  <!-- v-for -->
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="(task,index) in tasks" :key="task" >
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">X</button>
+    </li>
+  </ul>
+
+
+  <!-- v-on -->
+  <br>
+  <!-- <button v-on:click="changeStatus">Change Status</button> -->
+  <button @click="changeStatus">Change Status</button>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ vue_app/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var',"Poppins"],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
